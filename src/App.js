17cffebfe70ee8db92c0fac4cbd76fec78b05889
Christopher <mail@christophermaier.com.au>
2017-11-29 12:05:36 +1000

    Added states including search bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Header from './components/header'
+import {Input} from 'reactbulma'
 
 class App extends Component {
+
+  state = {
+    tasks: ['Do the washing', 'Become a JS Weapon'],
+    searchPhrase: ''
+  }
+
+  onChangeQuery = (event) => {
+    this.setState({
+      searchPhrase: event.target.value
+    })
+  }
+
+  addTask = (event) => {
+    // stop the browser from submitting the form
+    event.preventDefault();
+    // make a copy of the current tasks
+    const currentTasks = [...this.state.tasks];
+    // add the new task to our copy of tasks
+    currentTasks.push( this.state.searchPhrase );
+    // Update the state with the new tasks
+    this.setState({
+      tasks: currentTasks,
+      searchPhrase: ''
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+    const { tasks, searchPhrase } = this.state
+
+    return (<div className="App">
+      <Header title="Incomplete" totalIncomplete={ tasks.length }/>
+        <div>
+          <form onSubmit={ this.addTask }>
+            <Input primary large
+            placeholder="Search / Add a Todo"
+            value={ searchPhrase }
+            onChange={ this.onChangeQuery }
+            />
+          </form>
+          {
+            tasks
+            .filter(myTask => myTask.includes(searchPhrase))
+            .map(myTask => <p>{myTask}</p>)
+          }
+        </div>
+      </div>);
   }
 }
 
